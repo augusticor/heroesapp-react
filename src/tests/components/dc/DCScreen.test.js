1d5302216f1745ad0333f757c6b4d5027a636d9b
augusticor <rojascruzoscar@gmail.com>
2021-08-31 20:22:49 -0500

    Testing <MarvelScreen/> and <DashBoardRoutes/> components

--- src/tests/components/dc/DCScreen.test.js
+++ src/tests/components/dc/DCScreen.test.js
@@ -1,5 +1,5 @@
 const { shallow } = require('enzyme');
-const { default: DCScreen } = require('../../../components/dc/DCScreen');
+const { DCScreen } = require('../../../components/dc/DCScreen');
 
 describe('Tests on <DCScreen /> component', () => {
 	const wrapper = shallow(<DCScreen />);
